1d34cc94f45a90843b506dd35eb6af33c9ec2851
Pedro Martinez <pedro@charlottmx.onmicrosoft.com>
2025-07-23 15:36:17 -0600

    add extras

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { AuthModule } from './Modules/Core/auth/auth.module';
+import { UsersModule } from './Modules/Core/users/users.module';
 
+@Global()
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: join(__dirname, `../src/Configuration/.env`),
+      isGlobal: true,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  static Port: number;
+  constructor(private readonly configService: ConfigService) {
+    AppModule.Port = Number(this.configService.get('PORT') ?? 3000);
+   
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Integration ANIQ')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('ANIQ')
+    .addBearerAuth(
+      {
+        description: `Integration API Token <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'JWT',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header'
+      },
+      'Token'
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(AppModule.Port);
 }
 bootstrap();
